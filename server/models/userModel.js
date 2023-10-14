1d7549fdf20fe3fe2dd139cb2c7fcbb2f10f18c6
nickthorp1 <nickthorp1@outlook.com>
2023-10-14 13:04:54 +0100

    front-end set-up + backend schema

---
+++ server/models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    pic: {type: String, required: true, default:"https://www.wilsoncenter.org/sites/default/files/styles/large/public/media/images/person/james-person-1.webp"},
+
+
+},
+{timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User; 
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const dotenv = require('dotenv');
 
 dotenv.config();
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', cors(), (req, res) => {
+    res.send('This is coming from the server');
 })
