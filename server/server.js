d3601ac82bf5c0f5f478b184545bb4e7f6acf0b9
nickthorp1 <nickthorp1@outlook.com>
2023-10-15 16:51:49 +0100

    mongodb connect

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB();
+
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use(cors())
